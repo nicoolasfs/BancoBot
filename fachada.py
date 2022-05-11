7babbe2dc7e5f713d4a6d88eaceb2d3eda357e39
EBLANCO17 <55146667+EBlanco17@users.noreply.github.com>
2022-05-11 16:22:48 -0500

    Update fachada.py

--- fachada.py
+++ fachada.py
@@ -4,31 +4,31 @@ from fondos import Fondos
 
 
 class BancoFacade:
+    
+
     def __init__(self, numero_cuenta: int, codigo_seguridad: int):
+        
         self.numero_cuenta = numero_cuenta
         self.codigo_seguridad = codigo_seguridad
-        self.check_numero_cuenta = NumeroCuenta()
-        self.check_codigo_seguridad = CodigoSeguridad()
-        self.check_fondos = Fondos()
+        self.check_numero_cuenta = NumeroCuenta(self.numero_cuenta)
+        self.usuario = self.check_numero_cuenta.validar_numero_cuenta(self.numero_cuenta)
+        self.check_codigo_seguridad = CodigoSeguridad(self.usuario[1])
+        self.check_fondos = Fondos(self.usuario[2])
+        
+                        
 
+       
     def retirar_dinero(self, valor: float):
-        if (
-            self.check_numero_cuenta.validar_numero_cuenta(self.numero_cuenta)
-            and self.check_codigo_seguridad.validar_codigo_seguridad(
-                self.codigo_seguridad
-            )
-            and self.check_fondos.dinero_suficiente(valor)
-        ):
-            return (f"Transacción exitosa \n Retiro exitoso. \n Nuevo saldo: {self.check_fondos.dinero_cuenta}")
+        if (self.check_numero_cuenta.validar_numero_cuenta(self.numero_cuenta)[0] and self.check_codigo_seguridad.validar_codigo_seguridad(self.codigo_seguridad)):
+            if self.check_fondos.dinero_suficiente(valor):
+                return (f"Transacción exitosa \n Retiro exitoso. \n Nuevo saldo: {self.check_fondos.dinero_cuenta}")
+            else:
+                return (f"Transacción fallida \n Error: No tiene dinero suficiente \n Saldo actual: {self.check_fondos.dinero_cuenta}")
         else:
-            return (f"Transacción fallida \n Error: No tiene dinero suficiente \n Saldo actual: {self.check_fondos.dinero_cuenta}")
-
+            return "Transacción fallida"
+            
     def depositar_dinero(self, valor: float):
-        if self.check_numero_cuenta.validar_numero_cuenta(
-            self.numero_cuenta
-        ) and self.check_codigo_seguridad.validar_codigo_seguridad(
-            self.codigo_seguridad
-        ):
+        if self.check_numero_cuenta.validar_numero_cuenta(self.numero_cuenta)[0] and self.check_codigo_seguridad.validar_codigo_seguridad(self.codigo_seguridad):
             self.check_fondos.hacer_deposito(valor)
             return (f"Transacción exitosa \n Deposito exitoso. Nuevo saldo: {self.check_fondos.dinero_cuenta}")
         else:
