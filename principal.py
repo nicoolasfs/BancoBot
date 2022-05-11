c0988700e9061d76d91a48fb0f8b1757fc3ad037
EBLANCO17 <55146667+EBlanco17@users.noreply.github.com>
2022-05-11 16:23:51 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,10 +1,14 @@
 from fachada import BancoFacade
 
 if __name__ == "__main__":
-    cuenta = BancoFacade(numero_cuenta=123456, codigo_seguridad=1234)
+    cuenta = BancoFacade(666554433,8888)
+    
     retiro = cuenta.retirar_dinero(100.0)
     retiro1 = cuenta.retirar_dinero(900.0)
     retiro2 = cuenta.retirar_dinero(900.0)
     deposito = cuenta.depositar_dinero(200)
-    print(retiro, retiro1, retiro2, deposito)
+    print(retiro)
+    print(retiro1) 
+    print(retiro2)
+    print(deposito)
     
