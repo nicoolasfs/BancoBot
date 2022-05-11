bad664a1c30ebd01eacae1a85081bcd5b87a0a1b
EBLANCO17 <55146667+EBlanco17@users.noreply.github.com>
2022-05-11 16:21:16 -0500

    Update numero_cuenta.py

--- numero_cuenta.py
+++ numero_cuenta.py
@@ -1,10 +1,20 @@
 class NumeroCuenta:
-    def __init__(self):
-        self.numero_cuenta = 123456
+    _cuentas = {12345678:[1234,540000.0], 
+                987654321:[5678,100000.0], 
+                666554433:[8888,200000.0],
+                555443322:[9999,300000.0],
+                444332211:[4031,400000.0],
+                333221100:[1111,500000.0],
+                222110000:[2222,600000.0],
+                111000000:[3333,700000.0],
+                121000765:[4444,800000.0]}
+
+    def __init__(self, numero_cuenta: int):
+        self.numero_cuenta = numero_cuenta
 
     def validar_numero_cuenta(self, numero: int):
-        resultado = False
-        if numero == self.numero_cuenta:
-            resultado = True
-
+        resultado = [False,0,0]
+        if self.numero_cuenta in self._cuentas.keys():
+            resultado = [True,self._cuentas[self.numero_cuenta][0],self._cuentas[self.numero_cuenta][1]]
         return resultado
+    
